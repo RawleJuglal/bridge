f325b9bae8a2ab40b6de02d819d3fe0820d26b2a
Rawle Juglal <rawledjuglal@gmail.com>
2017-12-02 21:07:18 -0600

    changed(added the window reload on leagues for bandaid)

--- public/javascripts/leagues/leagues.controller.js
+++ public/javascripts/leagues/leagues.controller.js
@@ -6,16 +6,16 @@ class LeaguesCtrl {
     this._$location = $location;
     this._$window = $window;
 
-    // if(this._$window.localStorage){
-    //     if(!this._$window.localStorage.getItem('firstLoad')){
-    //         console.log('made second statement');
-    //         this._$window.localStorage['firstLoad'] = true;
-    //         this._$window.location.reload(true);
-    //     } else {
-    //         this._$window.localStorage.removeItem('firstLoad');
-    //     }
+    if(this._$window.localStorage){
+        if(!this._$window.localStorage.getItem('firstLoad')){
+            console.log('made second statement');
+            this._$window.localStorage['firstLoad'] = true;
+            this._$window.location.reload(true);
+        } else {
+            this._$window.localStorage.removeItem('firstLoad');
+        }
 
-    // }
+    }
   }
 
 }
