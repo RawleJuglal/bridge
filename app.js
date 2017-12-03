0885a8614e9660c98845d64d9d3cbe9af631ecb4
Rawle Juglal <rawledjuglal@gmail.com>
2017-12-02 21:23:46 -0600

    changed(path for favicon)

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.set('views', path.join(__dirname, 'build'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '/public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
     limit: '5mb',
